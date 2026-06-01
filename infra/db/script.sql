1718dbd2960d427f682ce18d2f4f64ff18cfc2fb
HenriqueVenso <henrique_venso@estudante.sesisenai.org.br>
2026-06-01 13:00:11 -0300

    add - Criação dos comandos para a inserção de banco de dados

--- infra/db/script.sql
+++ infra/db/script.sql
@@ -1,6 +1,8 @@
-CREATE DATABASE sistema_simples_m1;
+CREATE DATABASE sistema_simples_m1; 
+--  Comando utilizado para criar uma database
 
-USE sistema_simples_m1;
+USE sistema_simples_m1; 
+-- Está puxando a database criada, sendo necessário se referir ao nome dela quando inserir o "USE"
 
 CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -8,4 +10,8 @@ CREATE TABLE usuarios (
     senha VARCHAR(255) NOT NULL
 );
 
+-- Criação de uma tabela nova denominada "usuarios"
+
 INSERT INTO usuarios (usuario, senha) VALUES ('admin','123');
+
+-- Inserção das informações iniciais de usuario e senha na tabela usuarios
