e8c3b04ae2cc83d82477951af8bd0e662c03b52d
HenriqueVenso <henrique_venso@estudante.sesisenai.org.br>
2026-06-03 11:04:38 -0300

    add - Comentários para o logout.php

--- public/logout.php
+++ public/logout.php
@@ -1,7 +1,7 @@
 <?php
  
-session_start();
-    session_destroy();
+session_start(); // Inicia a sessão
+    session_destroy(); // Finaliza a sessão, jogando o usuário para a página de cadastro. A função do "session_destroy" é destuir todos os dados relacionados a sessão do usuário, incapacitando de voltar para outras páginas e o forçando a ir para a página de login ou cadastro.
     header("Location: ../index.php");
     exit();
    
